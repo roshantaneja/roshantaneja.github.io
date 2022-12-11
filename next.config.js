b9ee1ab97d8fa5db0dc0f3c33a74a7ec8d7346e1
Daroshi11260 <rytaneja@gmail.com>
2022-12-10 18:33:27 -0800

    ignore eslint in building to prod

--- next.config.js
+++ next.config.js
@@ -24,6 +24,11 @@ if (isGithubActions) {
 module.exports = {
   assetPrefix: assetPrefix,
   basePath: basePath,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     loader: 'imgix',
     path: 'https://daroshi11260.imgix.net',
