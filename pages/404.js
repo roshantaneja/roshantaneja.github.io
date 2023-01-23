86a5bcdd40d066c53508fb3bdce77fdfe5b9e15f
Yoshi <rytaneja@gmail.com>
2023-01-22 20:28:38 -0800

    removed the google analytics script cuz its breaking the site

--- pages/404.js
+++ pages/404.js
@@ -1,26 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Script from "next/script"
 import Link from 'next/link';
 export default function Custom404() {
     return (
         <div className={styles.container}>
             <Head>
-                <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-6302CZ25FK"/>
-                <Script
-                    id ='google-analytics'
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', 'G-6302CZ25FK');, {
-                        page_path: window.location.pathname,
-                        });`
-                    }}
-                />
                 <title>Roshan Taneja</title>
                 <meta name="description" content="can i puts my bawls in yo jaws" />
                 <link rel="icon" href="/favicon.ico" />
