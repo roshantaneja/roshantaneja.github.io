f6e13a84e8168663dd8cda5b944d488b9bc00fd5
Daroshi11260 <rytaneja@gmail.com>
2024-06-23 13:16:57 +0100

    roshan

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello, I'm <a href="./resume/Roshan Taneja Resume.pdf" download>Roshan Taneja!</a>
+          Hello, I'm <a href="./resume/Roshan Taneja Resume.pdf" download>Roshan!</a>
         </h1>
 
         <div>
