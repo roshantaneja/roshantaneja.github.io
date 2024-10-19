7c93ac7e93d10ca2b6bccf218d5cbfd89dca08e8
roshantaneja <rytaneja@gmail.com>
2024-10-19 14:27:42 -0700

    icons and trackers

--- pages/tanzania.js
+++ pages/tanzania.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function Tanzania() {
     return (
